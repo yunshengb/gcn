364b6ec51781ea475344c778fcf269ce626cee71
Yunsheng Bai <yba@ucla.edu>
2017-09-04 09:28:56 -0700

    Modify stub

--- gcn/analysis/node_classification/logistic_regression.py
+++ gcn/analysis/node_classification/logistic_regression.py
@@ -1,1 +1,22 @@
-print('Python 3 please!')
+import numpy as np
+
+
+def main():
+    print('Python 3 please!')
+
+    print('Load embeddings')
+
+    print(np.load('gcn_cora_emb.npy').shape)
+    print(np.load('node2vec_cora_emb.npy').shape)
+
+    print('Load ground-truths labels for cora')
+
+    print('Load features and append to embeddings')
+
+    print('Perform logistic regression')
+
+    print('Result')
+
+
+if __name__ == '__main__':
+    main()
