bcf3427bea90fb4dc362a7806baa2ddb5caae9b6
Yunsheng Bai <yba@ucla.edu>
2017-09-26 10:12:36 -0700

    Fix split bug

--- gcn/split.py
+++ gcn/split.py
@@ -2,29 +2,36 @@ import numpy as np
 import collections, random, os
 from utils import load_data
 
-LIMIT = 20000
-DATASET = "cora"
+LIMIT = 10000
+DATASET = "flickr"
+
 
 def main():
-    split_data = SplitData(DATASET)
+    split_data = SplitData()
     split_data.gen_split()
 
+
 class SplitData:
-    def __init__(self, dataset):
+    def __init__(self):
         self.visited = set()
         self.adj = None
-        if dataset == "cora":
+        if DATASET == "cora":
             self.adj = read_cora()
-
+        elif DATASET == "flickr":
+            self.adj = read_flickr()
         self.neigh_map = self._gen_neigh_map()
         self.folder_id = 0
         self.total_nodes = 0
+        os.system('rm -rf data/{}'.format(DATASET))
 
     def _gen_neigh_map(self):
+        print('Gen neighbor map: Getting edges')
         rows, cols = np.where(self.adj == 1)
         dic = collections.defaultdict(list)
+        print('Gen neighbor map: Iterating')
         for idx, row in enumerate(rows):
             dic[row].append(cols[idx])
+        print('Generated neighbor map')
         return dic
 
     def gen_split(self):
@@ -55,7 +62,7 @@ class SplitData:
                 self._add_to_id_map(real_to_fake, fake_to_real, cur)
                 real_cnt += 1
                 self.visited.add(cur)
-                for j in self.neigh_map[i]:
+                for j in self.neigh_map[cur]:
                     if j not in local_visited:
                         local_visited.add(j)
                         # Add cur's neighbors to the extended area.
@@ -70,12 +77,13 @@ class SplitData:
         real_to_fake[id] = len(real_to_fake)
         fake_to_real[len(fake_to_real)] = id
 
-
     def _check_neighbor(self, id):
         # check if i's neighor is less than LIMIT, otherwise randomly\
         # drop some of i's neighbors
         if self._get_degree(id) > LIMIT:
-            raise RuntimeError("For node#{}, Degree:{} > LIMIT".format(id, self._get_degree(id)))
+            raise RuntimeError("For node#{}, Degree:{} > LIMIT".format(id,
+                                                                       self._get_degree(
+                                                                           id)))
         else:
             return True
 
@@ -91,25 +99,32 @@ class SplitData:
         return len(fake_to_real) + self._get_degree(id) + 1 > LIMIT
 
     def _dump_to_disk(self, fake_to_real, real_to_fake, real_cnt):
-        path = "data/{}/{}".format(DATASET,self.folder_id)
+        path = "data/{}/{}".format(DATASET, self.folder_id)
         os.system("mkdir -pv {}".format(path))
-        print("Create Folder #{}".format(self.folder_id))
         self.folder_id += 1
-        new_adj = np.zeros((len(fake_to_real),len(fake_to_real)))
+        new_adj = np.zeros((len(fake_to_real), len(fake_to_real)))
         for real, fake in real_to_fake.items():
             for real_neigh in self.neigh_map[real]:
                 if real_neigh in real_to_fake:
                     new_adj[fake][real_to_fake[real_neigh]] = 1
-        np.save("{}/adj.npy".format(path),new_adj)
-        np.save("{}/dim.npy".format(path),real_cnt)
+        np.save("{}/adj.npy".format(path), new_adj)
+        np.save("{}/dim.npy".format(path), real_cnt)
         np.save("{}/map.npy".format(path), fake_to_real)
         self.total_nodes += real_cnt
-        print (self.total_nodes)
+        print('{} nodes; total: {}'.format(real_cnt, self.total_nodes))
+        print('Nodes: {}\n'.format(real_to_fake.keys()))
 
 
 def read_cora():
     adj, _, _, _, _, _, _, _ = load_data("cora", 0)
     return adj.todense()
+
+def read_flickr():
+    print('Loading flickr')
+    adj, _, _, _, _, _, _, _ = load_data("flickr", 0)
+    print('Loaded flickr')
+    return adj.todense()
+
 
 if __name__ == "__main__":
     main()
